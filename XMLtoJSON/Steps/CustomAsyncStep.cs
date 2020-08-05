45982309858e1c34dd4b76be86391bed7ace13d9
jon <jon.hallam@managed-desktop.com>
2020-08-05 09:52:24 +0100

    init
    init

---
+++ XMLtoJSON/Steps/CustomAsyncStep.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 
+/// </summary>
+namespace XMLtoJSON.Steps
+{
+    class CustomAsyncStep
+    {
+    }
+}
