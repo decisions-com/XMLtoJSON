28f5b73e628e8aacf77fabd3daa7bf7e2c3124c0
jon <jon.hallam@managed-desktop.com>
2020-08-05 12:42:49 +0100

    Add coded and added ModuleZip

---
+++ XMLtoJSON/XMLtoJsonClass.cs
@@ -0,0 +1,27 @@
+﻿using DecisionsFramework.Design.Flow;
+using DecisionsFramework.Design.Flow.CoreSteps.XML;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XMLtoJSON
+{
+    [AutoRegisterMethodsOnClass(true, "Data/XML")]
+    public class XMLtoJsonClass
+    {
+
+        public string ConvertXmlToJson (string XML)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(XML);
+
+            string json = JsonConvert.SerializeXmlNode(doc);
+            return json;
+        }
+
+    }
+}
--- msbuild.bat
+++ msbuild.bat
@@ -7,8 +7,8 @@ echo Running: "%programfiles(x86)%\MSBuild\14.0\bin\msbuild.exe" build.proj
 "%programfiles(x86)%\MSBuild\14.0\bin\msbuild.exe" build.proj
 
 pause
-net stop "Service Host Manager Watcher"
-net stop "Service Host Manager"
-copy .\XMLtoJSON.zip "c:\Program Files\Decisions\Decisions Services Manager\CustomModules" /Y
-net start "Service Host Manager"
+REM net stop "Service Host Manager Watcher"
+REM net stop "Service Host Manager"
+REM copy .\XMLtoJSON.zip "c:\Program Files\Decisions\Decisions Services Manager\CustomModules" /Y
+REM net start "Service Host Manager"
 pause
